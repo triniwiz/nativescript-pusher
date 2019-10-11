0c117d0541ab6734b3b675fc443390a6c184cdee
Osei Fortune <fortune.osei@yahoo.com>
2019-10-11 00:33:56 -0400

    fix(ios): channels

--- channels/interfaces.ts
+++ channels/interfaces.ts
@@ -1,24 +1,26 @@
 import { ConnectionStatus } from './enums';
 
 export interface ChannelEventMap {
-  binding: any;
-  callback: Function;
+    binding: any;
+    callback: Function;
 }
+
 export interface Options {
-  activityTimeout?: number;
-  authorizer?: string;
-  cluster?: string;
-  encrypted?: boolean;
-  host?: string;
-  pongTimeout?: number;
-  port?: number;
-  autoReconnect?: boolean;
-  authEndpoint?: string;
-  wsPort?: number;
-  wssPort?: number;
+    activityTimeout?: number;
+    cluster?: string;
+    encrypted?: boolean;
+    host?: string;
+    pongTimeout?: number;
+    port?: number;
+    autoReconnect?: boolean;
+    authEndpoint?: string;
+    authorizer?: (channel, options) => { authorize: (socketId, callback) => void };
+    wsPort?: number;
+    wssPort?: number;
+    auth?: { headers?: Object, params?: Object }
 }
 
 export interface ConnectionStatusEvent {
-  current: ConnectionStatus;
-  previous: ConnectionStatus;
+    current: ConnectionStatus;
+    previous: ConnectionStatus;
 }
